83c56bc89e6f16499d60f9f753a6a92049d73f6a
ahmedirheem <ahmedirheem2003@gmail.com>
2023-02-02 15:08:23 +0200

    File Structure

---
+++ gpt3-react/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+import {Footer, Blog, Features, Possibility, Header, WhatGPT3} from './containers'
+import {Navbar, CTA, Brand} from './components'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
